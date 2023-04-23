a88ec8e2f923c925958779c513b269f7f68e9bab
sudo_sid <lavoiesidney@gmail.com>
2023-04-23 14:14:30 -0400

    v0.0.2 - DHCP

--- config_net.py
+++ config_net.py
@@ -2,24 +2,24 @@ from netmiko import *
 import re
 
 
-def init_net(team1, team2):
-    vert = re.findall("..?", team1)
-    jaune = re.findall("..?", team2)
-
-    scoreswitch = {
+scoreswitch = {
         'device_type': 'cisco_ios',
         'host': '10.0.100.1',
         'username': 'beta-admin',
         'password': 'b3t4b0ts!',
         'secret': 'b3t4b0ts!',
     }
+# Call to add network information. Must provide a 4 number string for ip address
+def init_net(team1, team2):
+    vert = re.findall("..?", team1)
+    jaune = re.findall("..?", team2)
+
+
 
     ssh = ConnectHandler(**scoreswitch)
     ssh.enable()
     ssh.config_mode()
     commands = [
-                'no ip dhcp pool vert',
-                'no ip dhcp pool jaune',
                 'interface vlan 10',
                 'ip address 10.%s.%s.4 255.255.255.0' % (vert[0], vert[1]),
                 'interface vlan 20',
@@ -27,7 +27,6 @@ def init_net(team1, team2):
                 'exit',
                 'ip dhcp excluded-address 10.%s.%s.1 10.%s.%s.10' % (vert[0], vert[1], vert[0], vert[1]),
                 'ip dhcp excluded-address 10.%s.%s.1 10.%s.%s.10' % (jaune[0], jaune[1], jaune[0], jaune[1]),
-                'no ip dhcp pool vert',
                 'ip dhcp pool vert',
                 'lease 3',
                 'network 10.%s.%s.0 255.255.255.0' % (vert[0], vert[1]),
@@ -42,3 +41,18 @@ def init_net(team1, team2):
                 ]
     ssh.send_config_set(commands)
     ssh.disconnect()
+
+
+# Call to remove all network information
+def reset_net():
+    ssh = ConnectHandler(**scoreswitch)
+    ssh.enable()
+    ssh.config_mode()
+    commands = ["interface vlan 10",
+                "no ip address",
+                "exit",
+                "interface vlan 20",
+                "no ip address",
+                'no ip dhcp pool vert',
+                'no ip dhcp pool jaune'
+                ]
