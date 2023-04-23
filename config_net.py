ee7cb4e11a356077d0f4918a09c0b9ac3bbcbc0b
sudo_sid <lavoiesidney@gmail.com>
2023-04-23 11:34:19 -0400

    v0.0.1

--- config_net.py
+++ config_net.py
@@ -5,7 +5,6 @@ import re
 def init_net(team1, team2):
     side1 = re.findall("..?", team1)
     side2 = re.findall("..?", team2)
-    print("side1: ", side1)
 
     scoreswitch = {
         'device_type': 'cisco_ios',
@@ -16,7 +15,11 @@ def init_net(team1, team2):
     }
 
     ssh = ConnectHandler(**scoreswitch)
-    ssh.send_command('configure terminal'
-                     'interface vlan 10'
-                     'ip address 10.%s.%s.4 255.255.255.0' %(side1[0],side1[1]))
+    ssh.enable()
+    ssh.config_mode()
+    commands = ['interface vlan 10',
+                'ip address 10.%s.%s.4 255.255.255.0' % (side1[0], side1[1]),
+                'interface vlan 20',
+                'ip address 10.%s.%s.4 255.255.255.0' % (side2[0], side2[1])]
+    ssh.send_config_set(commands)
     ssh.disconnect()
--- main.py
+++ main.py
@@ -4,4 +4,4 @@ from config_net import *
 httpPort = 8080
 
 init_db()
-init_net("0001", "0002")
+init_net("1234", "5678")
