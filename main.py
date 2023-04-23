0a7605ca9eec1526c110f59b97259a9178ba5f64
sudo_sid <lavoiesidney@gmail.com>
2023-04-23 12:26:04 -0400

    v0.0.2

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ from httpserver import *
 httpPort = 8080
 serverHostName = "localhost"
 
-run_server(serverHostName, httpPort)
 init_db()
 init_net("1010", "2020")
+run_server(serverHostName, httpPort)
