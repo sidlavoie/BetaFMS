5c2bcf21757a572631f3c544128c9ca49ddf004e
sudo_sid <lavoiesidney@gmail.com>
2023-04-25 21:07:03 -0400

    v0.0.4 - point d'access, regénération WPA et message debug. Ajout d'un joli spinner

--- config_net.py
+++ config_net.py
@@ -1,5 +1,6 @@
 from netmiko import *
 from db_main import *
+from misc import *
 import re
 
 last_vert = '0000'
@@ -38,79 +39,83 @@ def init_net(teamvert, teamjaune):
 
     # scoreswitch config
     print("Configuring scoreswitch for team %s and %s..." % (teamvert, teamjaune))
-    ssh = ConnectHandler(**scoreswitch)
-    ssh.enable()
-    ssh.config_mode()
-    commands = [
-                'interface vlan 10',
-                'ip address 10.%s.%s.4 255.255.255.0' % (vert[0], vert[1]),
-                'interface vlan 20',
-                'ip address 10.%s.%s.4 255.255.255.0' % (jaune[0], jaune[1]),
-                'exit',
-                'ip dhcp excluded-address 10.%s.%s.1 10.%s.%s.10' % (vert[0], vert[1], vert[0], vert[1]),
-                'ip dhcp excluded-address 10.%s.%s.1 10.%s.%s.10' % (jaune[0], jaune[1], jaune[0], jaune[1]),
-                'ip dhcp pool vert',
-                'lease 3',
-                'network 10.%s.%s.0 255.255.255.0' % (vert[0], vert[1]),
-                'default-router 10.%s.%s.4' % (vert[0], vert[1]),
-                'domain-name beta.local'
-                'exit',
-                'ip dhcp pool jaune',
-                'lease 3',
-                'network 10.%s.%s.0 255.255.255.0' % (jaune[0], jaune[1]),
-                'default-router 10.%s.%s.4' % (jaune[0], jaune[1]),
-                'domain-name beta.local'
-                ]
-    ssh.send_config_set(commands)
-    ssh.disconnect()
+    with Spinner():
+        ssh = ConnectHandler(**scoreswitch)
+        ssh.enable()
+        ssh.config_mode()
+        commands = [
+                    'interface vlan 10',
+                    'ip address 10.%s.%s.4 255.255.255.0' % (vert[0], vert[1]),
+                    'interface vlan 20',
+                    'ip address 10.%s.%s.4 255.255.255.0' % (jaune[0], jaune[1]),
+                    'exit',
+                    'ip dhcp excluded-address 10.%s.%s.1 10.%s.%s.10' % (vert[0], vert[1], vert[0], vert[1]),
+                    'ip dhcp excluded-address 10.%s.%s.1 10.%s.%s.10' % (jaune[0], jaune[1], jaune[0], jaune[1]),
+                    'ip dhcp pool vert',
+                    'lease 3',
+                    'network 10.%s.%s.0 255.255.255.0' % (vert[0], vert[1]),
+                    'default-router 10.%s.%s.4' % (vert[0], vert[1]),
+                    'domain-name beta.local'
+                    'exit',
+                    'ip dhcp pool jaune',
+                    'lease 3',
+                    'network 10.%s.%s.0 255.255.255.0' % (jaune[0], jaune[1]),
+                    'default-router 10.%s.%s.4' % (jaune[0], jaune[1]),
+                    'domain-name beta.local'
+                    ]
+        ssh.send_config_set(commands)
+        ssh.disconnect()
 
-    # get SSID and WPA key from the database
-    wifiVert = get_teamWifi(teamvert)
-    wifiJaune = get_teamWifi(teamjaune)
+        # get SSID and WPA key from the database
+        wifiVert = get_teamWifi(teamvert)
+        wifiJaune = get_teamWifi(teamjaune)
 
-    # ap config
+        # ap config
     print("Configuring beta-ap with ssid %s and ssid %s..." % (wifiVert[0], wifiJaune[0]))
-    ssh = ConnectHandler(**beta_ap)
-    ssh.enable()
-    ssh.config_mode()
-    commands = [
-        'dot11 ssid %s' % wifiVert[0],
-        'vlan 10',
-        'authentication open',
-        'authentication key-management wpa version 2',
-        'wpa-psk ascii %s' % wifiVert[1],
-        'dot11 ssid %s' % wifiJaune[0],
-        'vlan 20',
-        'authentication open',
-        'authentication key-management wpa version 2',
-        'wpa-psk ascii %s' % wifiJaune[1],
-        'interface dot11Radio 1',
-        'ssid %s' % wifiVert[0],
-        'ssid %s' % wifiJaune[0]
-    ]
-    ssh.send_config_set(commands)
-    ssh.disconnect()
+    with Spinner():
+        ssh = ConnectHandler(**beta_ap)
+        ssh.enable()
+        ssh.config_mode()
+        commands = [
+            'dot11 ssid %s' % wifiVert[0],
+            'vlan 10',
+            'authentication open',
+            'authentication key-management wpa version 2',
+            'wpa-psk ascii %s' % wifiVert[1],
+            'dot11 ssid %s' % wifiJaune[0],
+            'vlan 20',
+            'authentication open',
+            'authentication key-management wpa version 2',
+            'wpa-psk ascii %s' % wifiJaune[1],
+            'interface dot11Radio 1',
+            'ssid %s' % wifiVert[0],
+            'ssid %s' % wifiJaune[0]
+        ]
+        ssh.send_config_set(commands)
+        ssh.disconnect()
     print("Network init complete!")
 
 
 # Call to remove all network information
 def reset_net():
     # Reset AP
-    print("Network reset...")
-    ssh = ConnectHandler(**beta_ap)
-    ssh.enable()
-    output = ssh.send_command_timing("configure replace nvram:startup-config")
-    if 'This' in output:
-        output += ssh.send_command_timing("y")
-    ssh.disconnect()
-    print("AP reset complete!")
+    print("Network reset requested...")
+    with Spinner():
+        ssh = ConnectHandler(**beta_ap)
+        ssh.enable()
+        output = ssh.send_command_timing("configure replace nvram:startup-config")
+        if 'This' in output:
+            output += ssh.send_command_timing("y")
+        ssh.disconnect()
 
-    # Reset scoreswitch
-    ssh = ConnectHandler(**scoreswitch)
-    ssh.enable()
-    output = ssh.send_command_timing("configure replace nvram:startup-config")
-    if 'This' in output:
-        output += ssh.send_command_timing("y")
-    ssh.disconnect()
+    print("AP reset complete!")
+    with Spinner():
+        # Reset scoreswitch
+        ssh = ConnectHandler(**scoreswitch)
+        ssh.enable()
+        output = ssh.send_command_timing("configure replace nvram:startup-config")
+        if 'This' in output:
+            output += ssh.send_command_timing("y")
+        ssh.disconnect()
     print("Scoreswitch reset complete!")
 
