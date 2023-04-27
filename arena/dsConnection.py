ab583d9c7e0a3a203603e3b825ce663557e1301f
sudo_sid <lavoiesidney@gmail.com>
2023-04-27 19:28:00 -0400

    ACL et test connexion DS

--- arena/dsConnection.py
+++ arena/dsConnection.py
@@ -11,18 +11,21 @@ def test():
         vert_ip = 0
         jaune_ip = 0
 
-        while vert_ip == 0 or jaune_ip == 0:
-            s.bind((HOST, PORT))
-            s.listen()
-            conn, addr = s.accept()
-            with conn:
-                if '12.34' in addr and vert_ip == 0:
-                    vert_ip = addr
-                    print ("vert: %s", vert_ip)
-                elif '46.78' in addr and jaune_ip == 0:
-                    jaune_ip = addr
-                    print ("jaune: %s", jaune_ip)
-                else:
-                    print('waiting for address')
-                    sleep(1)
+        while True:
+            if vert_ip == 0 or jaune_ip == 0:
+                s.bind((HOST, PORT))
+                s.listen()
+                conn, addr = s.accept()
+                with conn:
+                    if '12.34' in addr and vert_ip == 0:
+                        vert_ip = addr
+                        print ("vert: %s", vert_ip)
+                    elif '46.78' in addr and jaune_ip == 0:
+                        jaune_ip = addr
+                        print ("jaune: %s", jaune_ip)
+                    else:
+                        print('waiting for address')
+                        sleep(1)
+            else:
+                break
 
