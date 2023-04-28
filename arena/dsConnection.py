64d1ad8618b5f2189a27cfd17394b2bba633a4f3
sudo_sid <lavoiesidney@gmail.com>
2023-04-27 21:01:06 -0400

    ACL et test connexion DS

--- arena/dsConnection.py
+++ arena/dsConnection.py
@@ -1,18 +1,30 @@
 import socket
+import re
 
 
-def dsDiscover():
-    host = '10.0.100.5'
-    port = 1750
+def dsDiscover(vert, jaune):
+    vert_ds_ip = '0'
+    jaune_ds_ip = '0'
+    teamIPvert = re.findall("..?", vert)
+    teamIPjaune = re.findall("..?", jaune)
 
     s = socket.socket()
 
-    s.bind((host, port))
+    s.bind(('10.0.100.5', 1750))
     s.listen()
-
-    print("Server started! Waiting...")
 
     while True:
         conn, addr = s.accept()
-        print("ip: ", addr)
-        conn.close()
+        if vert_ds_ip == '0' or jaune_ds_ip == '0':
+            if teamIPvert in addr[0]:
+                vert_ds_ip = addr[0]
+                print("vert_ds_ip: ", vert_ds_ip)
+            elif teamIPjaune in addr[0]:
+                jaune_ds_ip = addr[0]
+                print("vert_ds_ip: ", jaune_ds_ip)
+            else:
+                print("No DS yet!")
+
+        else:
+            print("vert_ds_ip: ", vert_ds_ip, " jaune_ds_ip: ", jaune_ds_ip)
+            break
