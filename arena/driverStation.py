3dc7bc8107edc6d430f4bedc0520b57cc8d26519
sudo_sid <lavoiesidney@gmail.com>
2023-05-19 14:07:49 -0400

    Mis le packetcount à 0 initialement pour ne pas avoir une valeur none

--- arena/driverStation.py
+++ arena/driverStation.py
@@ -19,7 +19,7 @@ class DriverStation:
         self.seconds_since_last_robot_link = None
         self.last_packet_time = None
         self.last_robot_linked_time = None
-        self.packet_count = None
+        self.packet_count = 0
         self.missed_packet_offset = None
         self.tcp_conn = None
         self.udp_conn = None
