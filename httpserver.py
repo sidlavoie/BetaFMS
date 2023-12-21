361383f1f7247eff270f77aab64c4711e45bf996
sudo_sid <lavoiesidney@gmail.com>
2023-12-21 16:03:14 -0500

    v0.1.3 - Correction de la regénération d'horaire et ajout de contrôle d'horaire. ajout de test de chargement de match. Ajout de pauses et détection des pauses pour le chargement de match.

--- httpserver.py
+++ httpserver.py
@@ -160,7 +160,8 @@ class MyServer(BaseHTTPRequestHandler):
             elif self.path == '/loadnextmatch':
                 loadNextMatch()
 
-                self.send_response(200)
+                self.send_response(302)
+                self.send_header("Location", "/")
                 self.end_headers()
 
             elif self.path == '/teamadd':
