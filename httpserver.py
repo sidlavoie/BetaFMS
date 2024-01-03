f2f6dce1ea1b3aacb2728cfb60cc187157989485
sudo_sid <lavoiesidney@gmail.com>
2024-01-03 17:25:09 -0500

    v0.1.4 - Changements mineurs et changement de la page d'erreur

--- httpserver.py
+++ httpserver.py
@@ -24,6 +24,7 @@ class MyServer(BaseHTTPRequestHandler):
                     html_content = html_file.read()
 
                 self.wfile.write(bytes(html_content, "utf-8"))
+                return
 
             elif self.path == '/favicon.ico':
                 with open('www/static/images/favicon.ico', 'rb') as favicon_file:
@@ -34,8 +35,9 @@ class MyServer(BaseHTTPRequestHandler):
                 self.send_header("Content-Length", str(len(favicon_content)))
                 self.wfile.write(favicon_content)
                 self.end_headers()
+                return
 
-            elif self.path == '/add_team?':
+            elif self.path.startswith("/add_team"):
                 template = env.get_template("www/add_team.html")
 
                  # Example team data
@@ -45,12 +47,13 @@ class MyServer(BaseHTTPRequestHandler):
 
                 self.wfile.write(rendered_template.encode('utf-8'))
                 self.end_headers()
+                return
 
-            elif self.path == '/scheduleControl?':
+            elif self.path.startswith('/scheduleControl'):
                 template = env.get_template("www/scheduleControl.html")
 
                 rendered_template = template.render(
-                    day=f"{schedule_conf.day.year}-{schedule_conf.day.month}-{schedule_conf.day.day}",
+                    day=schedule_conf.day.strftime("%Y-%m-%d"),
                     starttime=schedule_conf.starttime.strftime('%H:%M'),
                     ambreaktime=schedule_conf.ambreaktime.strftime('%H:%M'),
                     lunchtime=schedule_conf.lunchtime.strftime('%H:%M'),
@@ -65,9 +68,10 @@ class MyServer(BaseHTTPRequestHandler):
                 self.send_header('Content-type', 'text/html')
                 self.wfile.write(rendered_template.encode('utf-8'))
                 self.end_headers()
+                return
 
             else:
-                try:
+                try:  # Handle media content and static pages (pictures, sounds, scripts...)
                     if self.path.find(".jpeg") != -1:  # Handle pictures
                         if self.path[0] == '/':
                             self.path = self.path[1:]
@@ -82,6 +86,8 @@ class MyServer(BaseHTTPRequestHandler):
                         self.send_header("Content-Length", str(len(image_content)))
                         self.wfile.write(image_content)
                         self.end_headers()
+
+                        return
 
                     if self.path.find(".wav") != -1:  # Handle audio
                         if self.path[0] == '/':
@@ -98,12 +104,13 @@ class MyServer(BaseHTTPRequestHandler):
                         self.wfile.write(audio_content)
                         self.end_headers()
 
+                        return
+
                     if self.path.find(".css") != -1:  # Handle css
                         if self.path[0] == '/':
                             self.path = self.path[1:]
                         if self.path.find("www/css/") == -1:
                             self.path = "www/css/" + self.path
-                        print (self.path)
 
                         with open(self.path, 'rb') as css_file:
                             css_content = css_file.read()
@@ -113,6 +120,25 @@ class MyServer(BaseHTTPRequestHandler):
                         self.send_header("Content-Length", str(len(css_content)))
                         self.wfile.write(css_content)
                         self.end_headers()
+
+                        return
+
+                    if self.path.find(".js") != -1:  # Handle JS
+                        if self.path[0] == '/':
+                            self.path = self.path[1:]
+                        if self.path.find("www/scripts/") == -1:
+                            self.path = "www/scripts/" + self.path
+
+                        with open(self.path, 'rb') as js_file:
+                            js_content = js_file.read()
+
+                        self.send_response(200)
+                        self.send_header("Content-type", "application/javascript")
+                        self.send_header("Content-Length", str(len(js_content)))
+                        self.wfile.write(js_content)
+                        self.end_headers()
+
+                        return
 
                     else:
                         # Append www and .html if it does not exist
@@ -213,7 +239,7 @@ class MyServer(BaseHTTPRequestHandler):
                         self.end_headers()
                         self.wfile.write(json.dumps("Nombre de symboles trop grand dans un des champs!").encode('utf-8'))
 
-            if self.path == '/removeteam':
+            elif self.path == '/removeteam':
                 form = cgi.FieldStorage(
                     fp=self.rfile,
                     headers=self.headers,
@@ -222,6 +248,19 @@ class MyServer(BaseHTTPRequestHandler):
 
                 # Retrieve form values
                 deleteTeam(form.getvalue("delteam"))
+                self.send_response(302)
+                self.send_header("Location", "/add_team?")
+                self.end_headers()
+
+            elif self.path == '/wparegen':
+                form = cgi.FieldStorage(
+                    fp=self.rfile,
+                    headers=self.headers,
+                    environ={'REQUEST_METHOD': 'POST', 'CONTENT_TYPE': self.headers['Content-Type'], }
+                )
+
+                # Retrieve form values
+                regenerate_wpa(form.getvalue("wpateam"))
                 self.send_response(302)
                 self.send_header("Location", "/add_team?")
                 self.end_headers()
@@ -237,21 +276,15 @@ class MyServer(BaseHTTPRequestHandler):
         self.send_response(status_code)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
-        error_page = f"""
-            <html>
-            <head>
-                <title>Error</title>
-            </head>
-            <body>
-                <h2>Error: {status_code}</h2>
-                <p>{message}</p>
-                <form action="/" method="get">
-                <input type="submit" value="Retour">
-                </form>
-            </body>
-            </html>
-            """
-        self.wfile.write(error_page.encode('utf-8'))
+
+        # Serve the error page directly
+        with open('www/error.html', 'r', encoding='utf-8') as error_page_file:
+            error_page_content = error_page_file.read()
+
+        # Replace the placeholder with the actual error message
+        error_page_content = error_page_content.replace('{{errorMessage}}', message)
+
+        self.wfile.write(bytes(error_page_content, 'utf-8'))
 
 
 def run_server(hostname, port):
--- www/scripts/errorHandling.js
+++ www/scripts/errorHandling.js
@@ -1,26 +1,46 @@
 // Function to handle the response from the server
 function handleErrorResponse(response) {
-  if (response.ok) {
-      // Handle successful response here
-  } else {
-    response.json().then(data => {
-      // Check if the error property exists in the JSON
-      if (data && data.error) {
-        // Generate HTML content for the error message and return button
-        var errorPageHTML = '<div id="errorPage" style="text-align: center; padding: 20px;">';
-        errorPageHTML += '<h2 style="color: #cc0000;">Error</h2>';
-        errorPageHTML += '<p>' + data.error + '</p>';
-        errorPageHTML += '<button onclick="redirectToHome()">Return</button>';
-        errorPageHTML += '</div>';
+    if (response.ok) {
+        // Handle successful response here
+    } else {
+        response.json().then(data => {
+            // Check if the error property exists in the JSON
+            if (data && data.error) {
+                // Load the error page dynamically
+                loadErrorPage(data.error);
+            }
+        });
+    }
+}
 
-        // Replace the entire body content with the error page HTML
-        document.body.innerHTML = errorPageHTML;
-      }
-    });
-  }
+// Function to load the error page dynamically
+function loadErrorPage(errorMessage) {
+    var errorContainer = document.createElement('div');
+    errorContainer.id = 'errorContainer';
+    errorContainer.style.textAlign = 'center';
+    errorContainer.style.padding = '20px';
+
+    var errorHeader = document.createElement('h2');
+    errorHeader.style.color = '#cc0000';
+    errorHeader.textContent = 'Error';
+
+    var errorParagraph = document.createElement('p');
+    errorParagraph.textContent = errorMessage;
+
+    var returnButton = document.createElement('button');
+    returnButton.textContent = 'Return';
+    returnButton.onclick = redirectToHome;
+
+    errorContainer.appendChild(errorHeader);
+    errorContainer.appendChild(errorParagraph);
+    errorContainer.appendChild(returnButton);
+
+    // Replace the entire body content with the error container
+    document.body.innerHTML = '';
+    document.body.appendChild(errorContainer);
 }
 
 // Function to redirect to the home page
 function redirectToHome() {
-  window.location.href = '/';
+    window.location.href = '/';
 }
